5734361d8b7c7e6e84b957f91a94a19696eada7d
Stephan <stephan.erich@hotmail.com>
2018-06-22 09:14:35 +0200

    Change for january and february

--- src/main/java/dayofbirth/Zeller.java
+++ src/main/java/dayofbirth/Zeller.java
@@ -1,14 +1,13 @@
 package dayofbirth;
 
+
 import java.util.Scanner;
 
 /**
- *
  * This simple app will ask the user for specific input and puts this input in the Zeller formula
  * to print the day of birth.
  *
  */
-
 public class Zeller {
 
     public static void main(String[] args) {
@@ -21,19 +20,34 @@ public class Zeller {
         int year = input.nextInt();
 
         // Ask for the century
-        System.out.print("Enter the first two digits of the year you were born : ");
+        System.out.print("Enter the first two digits of the year you were born : \n");
         int century = input.nextInt();
 
         // Ask for the month
-        System.out.print("Enter the month you were: 1-12: ");
+        System.out.print("Enter the month you were: 1-12: \n");
         int month = input.nextInt();
 
         // Ask for the day
-        System.out.print("Enter the day of the month you were born: ");
+        System.out.print("Enter the day of the month you were born: \n");
         int day = input.nextInt();
 
+        // If the birthday is in the first two months of the year, we have to go back one year
+        switch(month) {
+            case 1:
+                month = 11;
+                year = year -1;
+                break;
+            case 2:
+                month = 12;
+                year = year -1;
+                break;
+            default:
+                month = month -2;
+        }
+
+
         // Create Zeller Formula
-        int f = (day + (((month - 2) * 26 - 2) / 10) + year + (year / 4) + (century / 4) + (5 * century)) % 7;
+        int f = (day + ((month * 26 - 2) / 10) + year + (year / 4) + (century / 4) + (5 * century)) % 7;
 
 
         String result = "The day you were born was: ";
@@ -61,6 +75,14 @@ public class Zeller {
             case 6:
                 System.out.print(result + "Saturday");
                 break;
+
+
+
         }
     }
 }
+
+
+
+
+
